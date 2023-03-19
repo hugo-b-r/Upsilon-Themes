562a0271bac1d7019e20140efc4d9816551cd27d
Hugo Berthet-Rambaud <hugoberthetrambaud@outlook.com>
2023-03-19 18:54:43 +0100

    feat: building for each theme
    cloning and changin dir at first and then building in a for loop while storing each simulator

--- building_simulators.py
+++ building_simulators.py
@@ -1,5 +1,7 @@
 from os import listdir
+import os
 from os.path import isfile, join, splitext
+import shutil
 files = [f for f in listdir("./") if isfile(join("./", f))]
 
 themes= [] #list the themes 
@@ -7,3 +9,20 @@ for file in files:
     split_file = splitext(file)
     if split_file[1] == '.json':
         themes.append(split_file[0])
+
+#set up building
+try:
+    os.mkdir("build")
+except:
+    print()
+os.chdir("build")
+os.system("git clone --recursive https://github.com/UpsilonNumworks/Upsilon.git")
+os.chdir("Upsilon")
+os.system("git checkout upsilon-dev") #building the dev branch as we are crazy
+
+
+for theme in themes:
+    print(f"building theme: {theme}")
+    os.system("make clean")
+    os.system(f"make PLATFORM=simulator THEME_NAME={theme} THEME_REPO=https://github.com/hugo-b-r/Upsilon-Themes.git -j8 > ../{theme}_log.txt")
+    shutil.move('./output/release/simulator/linux/epsilon.bin', f'../{theme}.bin')
