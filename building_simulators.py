0a8f7f90fcd0eddd3fa4092e40c71e9a4c24ec6f
Hugo Berthet-Rambaud <hugoberthetrambaud@outlook.com>
2023-03-19 18:35:46 +0100

    feat: get the list of every theme

--- building_simulators.py
+++ building_simulators.py
@@ -1,3 +1,9 @@
 from os import listdir
-from os.path import isfile, join
+from os.path import isfile, join, splitext
 files = [f for f in listdir("./") if isfile(join("./", f))]
+
+themes= [] #list the themes 
+for file in files:
+    split_file = splitext(file)
+    if split_file[1] == '.json':
+        themes.append(split_file[0])
